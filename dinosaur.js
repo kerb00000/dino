cabe79407a9bfcdfc4e389ecce642759019c3828
Nimaan <nimaan.pannu@columbiachristian.com>
2026-04-09 09:20:01 -0700

    Adjust dinosaur position, create sprite coord array

--- dinosaur.js
+++ dinosaur.js
@@ -4,35 +4,47 @@ export class Dinosaur {
         this.y = 100
         this.dy = 0
         document.addEventListener("keydown", this.keydown.bind(this))
+
         this.sprite_sheet = new Image()
         this.sprite_sheet.src = "dinosprites.png"
-         this.sprite = { x: 1514, y: 2, w: 88, h: 94 }
-        //top left and width and height 4 sprite
+        this.sprites = {
+            "standing": { x: 1338, y: 2, w: 88, h: 94 },
+            "walking1": { x: 1514, y: 2, w: 88, h: 94 },
+            "walking2": { x: 1602, y: 2, w: 88, h: 94 },
+        }
 
     }
-
 
     keydown(event) {
         console.log("key pressed", event)
         this.dy += -20
 
-       
     }
 
 
     draw(ctx) {
+
+
         ctx.fillStyle = "rgb(255, 145, 0"
         ctx.beginPath()
         ctx.arc(this.x, this.y, 10, 0, Math.PI * 2)
         ctx.fill()
 
-        
+        var current_sprite = "walking2"
+        var sprite = this.sprites[current_sprite]
+
+        this.scale = 0.6
         ctx.drawImage(this.sprite_sheet,
-            this.sprite.x, this.sprite.y,
-            this.sprite.w, this.sprite.h,
-            this.x, this.y,
-           this.sprite.w, this.sprite.h
+            sprite.x,sprite.y,
+            sprite.w,sprite.h,
+            this.x - sprite.w * this.scale / 2,
+            this.y - sprite.h * this.scale,
+            sprite.w * this.scale,
+            sprite.h * this.scale
         )
+
+
+
 
         this.y += this.dy;
         this.dy += 3
