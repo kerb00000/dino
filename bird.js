cd48823b4e6290d946e1ec3687722677cfa71d6e
Alex Thomas <alex.thomas@columbiachristian.com>
2026-04-20 08:44:52 -0700

    changed bird and cactus

--- bird.js
+++ bird.js
@@ -1,22 +1,10 @@
+import { sprite } from './sprite.js'
+
 export class Bird {
     constructor(game) {
-        this.x = 600 
+        super(game)
+        this.x = 600
         this.y = 200
-        this.game = game
     }
-    draw(ctx){
-        
-        var current_sprite = "bird2"
-        var sprite = this.game.sprites[current_sprite]
 
-        this.scale = 0.6
-        ctx.drawImage(this.game.sprite_sheet,
-            sprite.x,sprite.y,
-            sprite.w,sprite.h,
-            this.x - sprite.w * this.scale / 2,
-            this.y - sprite.h * this.scale,
-            sprite.w * this.scale,
-            sprite.h * this.scale
-        )
-    }
 }
--- cactus.js
+++ cactus.js
@@ -1,31 +1,12 @@
+import { sprite } from './sprite.js'
+
 export class Cactus {
     constructor(game) {
+        super(game)
         this.x = 315    //right side of
-        this.y = 200    //floor level
-        this.game = game
+        this.y = 200    //floor level 
     }
 
-    draw(ctx) {
-
-    ctx.fillStyle = "rgb(0, 0, 0"
-    ctx.beginPath()
-    
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 4;
-    ctx.strokeRect(300, 120, 30, 80);
-
-    var current_sprite = "cactus1"
-        var sprite = this.game.sprites[current_sprite]
-
-        this.scale = 0.6
-        ctx.drawImage(this.game.sprite_sheet,
-            sprite.x,sprite.y,
-            sprite.w,sprite.h,
-            this.x - sprite.cx * this.scale,
-            this.y - sprite.cy * this.scale,
-            sprite.w * this.scale,
-            sprite.h * this.scale
-        )
-    }
+  
 }
 
